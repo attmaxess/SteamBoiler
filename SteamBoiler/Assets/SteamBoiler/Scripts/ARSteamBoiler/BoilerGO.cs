36df3b1a344a811d801e2627c9a6e98e98f2327a
DESKTOP-5D9G5PK\Asrock <thandh@beetsoft.com.vn>
2019-05-15 17:03:59 +0700

    thay code transparent

--- SteamBoiler/Assets/SteamBoiler/Scripts/ARSteamBoiler/BoilerGO.cs
+++ SteamBoiler/Assets/SteamBoiler/Scripts/ARSteamBoiler/BoilerGO.cs
@@ -33,16 +33,25 @@ public class BoilerGO : MonoBehaviour
     void SetOuter(bool Set)
     {
         if (outers.Count == 0) return;
+
+        ChangeMode changeMode = GetComponent<ChangeMode>();
+
         foreach (Transform o in outers)
         {
             MeshRenderer mr = o.gameObject.GetComponent<MeshRenderer>();
             if (mr != null)
             {
+                changeMode.mr = mr;
+                Material newMaterial = Set ? changeMode.GetOpaque() : changeMode.GetTransparent();
+                mr.material = newMaterial;
+
                 Color currentColor = mr.material.color;
 
-                mr.material.SetFloat("_Mode", Set ? 0f : 3f);
+                //mr.material.SetFloat("_Mode", Set ? 0f : 3f);
                 mr.material.color =
                     new Color(currentColor.r, currentColor.g, currentColor.b, Set ? 1 : 0);
+
+                //mr.UpdateGIMaterials();
             }            
         }
     }
---
+++ SteamBoiler/Assets/SteamBoiler/Scripts/Material/ChangeMode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMode : MonoBehaviour
+{
+    public MeshRenderer mr = null;
+
+    public Material GetOpaque()
+    {
+        Material material = mr.material;
+        material.SetFloat("_Mode", 0);
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+        material.SetInt("_ZWrite", 1);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.DisableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = -1;
+        return material;
+    }
+
+    public Material GetTransparent()
+    {
+        Material material = mr.material;
+        material.SetFloat("_Mode", 3);
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("_ZWrite", 0);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.DisableKeyword("_ALPHABLEND_ON");
+        material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = 3000;
+        return material;
+    }
+}
