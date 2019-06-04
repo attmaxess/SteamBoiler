ed5250c46f22644085380edad957bf165a0360e0
tunght <tunght@beetsoft.com.vn>
2019-06-04 09:19:49 +0700

    - Gesture Boiler - Change Room Boiler

---
+++ SteamBoiler/Assets/SteamBoiler/Scripts/GestureBoiler/CameraController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	[SerializeField] ThirdPersonCamera tpCamera;
+	Vector3 posOld;
+	Vector3 eulerAnglesOld;
+	enum TypeCamera {
+		LOOK_DOWN,
+		LOOK_HORIZONTAL
+	}
+	TypeCamera currentType;
+	bool canDrag = true;
+	public bool CanDrag {
+		get { return canDrag; }
+		set { canDrag = value; }
+	}
+	void Start () {
+		currentType = TypeCamera.LOOK_HORIZONTAL;
+		tpCamera.Pitch = 40;
+	}
+	public void SetCameraLookDown () {
+		tpCamera.Pitch = 90;
+	}
+
+	public void SetCameraLookHorizontal () {
+		tpCamera.Pitch = 0;
+	}
+
+	private void Update () {
+
+		if (!canDrag) return;
+#if UNITY_EDITOR
+		if (Input.GetMouseButton (0)) {
+			tpCamera.Yaw += Input.GetAxis ("Mouse X") * 10;
+			tpCamera.Pitch -= Input.GetAxis ("Mouse Y") * 10;
+		}
+
+		tpCamera.Distance -= Input.GetAxis ("Mouse ScrollWheel") * 10;
+#else
+		CalculateTouchDrag ();
+		CalculatePinchZoom ();
+		const float ROTATING_SPEED = .2f;
+		const float ZOOMING_SPEED = .07f;
+
+		tpCamera.Yaw += HorizontalDrag * ROTATING_SPEED;
+		tpCamera.Pitch += VerticalDrag * ROTATING_SPEED;
+		tpCamera.Distance += PinchZoomValue * ZOOMING_SPEED;
+#endif
+
+	}
+	public float PinchZoomValue { get; set; }
+
+	public float HorizontalDrag { get; set; }
+	public float VerticalDrag { get; set; }
+
+	void CalculateTouchDrag () {
+
+		HorizontalDrag = VerticalDrag = 0;
+
+		if (Input.touchCount == 1 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+
+			// Move object across XY plane
+			HorizontalDrag = touchDeltaPosition.x;
+			VerticalDrag = -touchDeltaPosition.y;
+		}
+	}
+
+	void CalculatePinchZoom () {
+
+		PinchZoomValue = 0;
+
+		// If there are two touches on the device...
+		if (Input.touchCount == 2 &&
+			(Input.GetTouch (0).phase == TouchPhase.Moved || Input.GetTouch (1).phase == TouchPhase.Moved)) {
+
+			// Store both touches.
+			Touch touchZero = Input.GetTouch (0);
+			Touch touchOne = Input.GetTouch (1);
+
+			// Find the position in the previous frame of each touch.
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			// Find the magnitude of the vector (the distance) between the touches in each frame.
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			// Find the difference in the distances between each frame.
+			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+			PinchZoomValue = deltaMagnitudeDiff;
+		}
+	}
+}
---
+++ SteamBoiler/Assets/SteamBoiler/Scripts/GestureBoiler/ThirdPersonCamera.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[AddComponentMenu("Camera/ThirdPersonCamera")]
+[RequireComponent(typeof(Camera))]
+public class ThirdPersonCamera : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    public Transform Target {
+        get { return target; }
+        set { target = value; }
+    }
+
+    [SerializeField] float smoothing = 8;
+
+    [SerializeField] float lowerPitchLimit = -40;
+    [SerializeField] float upperPitchLimit = 80f;
+    [SerializeField] float minDistance = 1f;
+    [SerializeField] float maxDistance = 5f;
+
+    Camera _camera;
+
+    [Header("Default values")]
+    [SerializeField]
+    float defaultYaw = 200;
+    [SerializeField] float defaultPitch = 5;
+    [SerializeField] float defaultDistance = 0;
+    [SerializeField] bool resetWhenActive = true;
+
+    public float Yaw { get; set; }
+    public float Pitch { get; set; }
+    public float Distance { get; set; }
+
+    float yaw, pitch, distance;
+    public Camera Camera {
+        get {
+            if (!_camera)
+                _camera = GetComponent<Camera>();
+            return _camera;
+        }
+    }
+
+    private void OnEnable() {
+        if (resetWhenActive)
+            ResetToDefault();
+    }
+
+    public void ResetToDefault() {
+
+        if (defaultYaw >= 0)
+            yaw = Yaw = defaultYaw;
+
+        if (defaultPitch >= 0)
+            pitch = Pitch = defaultPitch;
+
+        if (defaultDistance >= 0) {
+            if (defaultDistance == 0)
+                defaultDistance = minDistance + (maxDistance - minDistance) / 4;
+            distance = Distance = defaultDistance;
+        }
+
+        LateUpdate();
+    }
+
+    void LateUpdate() {
+        if (target) {
+
+            // Clamping
+            Pitch = ClampAngle(Pitch, lowerPitchLimit, upperPitchLimit);
+            Distance = Mathf.Clamp(Distance, minDistance, maxDistance);
+
+            if (smoothing > 0) {
+                yaw = Mathf.Lerp(yaw, Yaw, Time.deltaTime * smoothing);
+                pitch = Mathf.Lerp(pitch, Pitch, Time.deltaTime * smoothing);
+                distance = Mathf.Lerp(distance, Distance, Time.deltaTime * smoothing);
+            } else {
+                yaw = Yaw;
+                pitch = Pitch;
+                distance = Distance;
+            }
+
+            Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+            Vector3 negDistance = Vector3.back * distance;
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max) {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
